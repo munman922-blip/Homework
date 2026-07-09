63b3de774c60518388c092e24fa5f871989df6d7
munman922-blip <munman922@gmail.com>
2026-07-09 17:28:16 +0800

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,7 @@
+a=(int(input("Enter a number")))
+b=(int(input("Enter a number")))
+total=a/b
+if total==b:
+   print("The square root of a is",b)
+else:
+  print("The square root is a decimal number")
