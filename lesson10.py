fa7d2d4b7fa79bb6a656b6d5a683aac28bc61e94
munman922-blip <munman922@gmail.com>
2026-07-15 12:55:14 +0800

    Add files via upload

---
+++ lesson10.py
@@ -0,0 +1,4 @@
+num_powered=int(input("Enter the number which is gonna be powered to the next you choose"))
+num_which_powering=int(input("Enter the number which is gonna be powered to the next you choose"))
+answer=num_powered**num_which_powering
+print("The answer is ",answer)
