006ec3252757a68e3f3863e3ef7b9edc45b96fb9
munman922-blip <munman922@gmail.com>
2026-07-21 16:10:28 +0800

    Add files via upload

--- Lesson1.py
+++ Lesson1.py
@@ -1,6 +1,11 @@
-a=13
-print("Mahi's birthday is in this amount of weeks which is ", a)
-print("Kian's birthday is in this amount of weeks which is ",a)
-print("Vivaan's birthday is in this amount of weeks which is ", a)
-print("Jude's birthday is in this amount of weeks which is ", a)
-print("Raj's birthday is in this amount of weeks which is ", a)
+a=1
+b=2
+c=3
+d=4
+e=5
+f=2026
+print("Mahi's birthday is on",a,b,f)
+print("Kian's birthday is on",c,d,f)
+print("Vivaan's birthday is on",e,e,f)
+print("Jude's birthday is on",d,a,f)
+print("Raj's birthday is on",b,b,f)
---
+++ lesson9.py
@@ -0,0 +1,7 @@
+choice = int(input("Enter 1 if you rather have a beach holiday or enter 0 if you would rather have a mountain holiday"))
+if choice == 1:
+    print("You want to go on a beach holiday")
+elif choice == 0:
+    print("You want to go to a mountain holiday")
+else:
+    print("Error please enter a valid number")
